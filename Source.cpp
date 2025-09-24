7213ddb4fce921021ed53d41888f9a1d69d1b1c6
ABHINAV JOY C <abhinavjoyc@gmail.com>
2025-09-24 17:22:33 +0530

    running and idle
    running and idle
    animation completed

--- Source.cpp
+++ Source.cpp
@@ -8,6 +8,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include<array>
+#include<math.h>
 
 struct SDLstate
 {
@@ -17,6 +18,13 @@ struct SDLstate
 	int width;
 	int logw;
 	int logh;
+	const bool* keys;
+	SDLstate() : keys(SDL_GetKeyboardState(nullptr) )
+	{
+
+	}
+
+
 };
 void cleanup(SDLstate& state);
 void initialize(SDLstate& state);
@@ -36,9 +44,11 @@ struct GameState {
 struct resource {
 
 	const int ANIM_PLAYER_IDLE = 0;
+	const int ANIM_PLAYER_RUN = 1;
 	std::vector<Animation> playerAnims;
 	std::vector<SDL_Texture*> textures;
 	SDL_Texture* texIdle;
+	SDL_Texture* texRun;
 	SDL_Texture* loadTexture(SDL_Renderer *renderer, std::string &filepath) {
 
 		SDL_Texture* tex = IMG_LoadTexture(renderer, filepath.c_str());
@@ -49,9 +59,18 @@ struct resource {
 
 	void load(SDLstate& state) {
 		playerAnims.resize(5);
-		playerAnims[ANIM_PLAYER_IDLE] = Animation(5, 1.6f);
-		std::string path = "data/soldierBoy.png";
-		texIdle = loadTexture(state.renderer,path );
+
+		//idle
+	;
+		playerAnims[ANIM_PLAYER_IDLE] = Animation(6, 1.6f);
+		std::string path = "data/warriorIdle.png";
+		texIdle = loadTexture(state.renderer, path);
+
+		//running
+		std::string path2 = "data/warrior.png";
+		playerAnims[ANIM_PLAYER_RUN] = Animation(6, 1.6f);
+		texRun = loadTexture(state.renderer, path2);
+
 	}
 	void unload() {
 		for (SDL_Texture* Tex : textures) {
@@ -63,7 +82,7 @@ struct resource {
 
 };
 void drawObject(const SDLstate& state, GameState& gs, GameObject& obj, float deltatime);
-
+void update(SDLstate& state, GameState& gs, GameObject& obj, resource& rs, float delta);
 int main(int argc, char* argv[])
 {
 	SDLstate state;
@@ -73,10 +92,14 @@ int main(int argc, char* argv[])
 	// game data 
 	GameState gs;
 	GameObject player;
+	
 	player.type = ObjectType::player;
+	player.data.player = PlayerData();
 	player.texture = res.texIdle;
 	player.animations = res.playerAnims;
 	player.currentAnimations = res.ANIM_PLAYER_IDLE;
+	player.acceleration = glm::vec2(300, 0);
+	player.maxSpeed = 100;
 	gs.layers[LAYER_IDX_CHARACTERS].push_back(player);
 	SDL_SetRenderLogicalPresentation(state.renderer, state.logw, state.logh, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 	const bool* key = SDL_GetKeyboardState(nullptr);
@@ -152,6 +175,7 @@ int main(int argc, char* argv[])
 			for (auto& layer : gs.layers) {
 				for (GameObject& obj : layer)
 				{
+					update(state, gs, obj,res, delta);
 					drawObject(state, gs, obj, delta);
 					
 				}
@@ -190,16 +214,18 @@ void cleanup(SDLstate& state)
 
 void drawObject(const SDLstate &state, GameState& gs, GameObject& obj, float deltatime) {
 	
-	float sprtiteSize = 32;
+	//float sprtiteSize = 44;
+	float spriteWidth = 69;
+	float spritelength = 44;
 	float srcX = (obj.currentAnimations != -1)
-		? obj.animations[obj.currentAnimations].currentFrame()* sprtiteSize
+		? obj.animations[obj.currentAnimations].currentFrame()* spriteWidth
 		: 0.0f;
 	SDL_FRect src
 	{
 		.x = srcX,
 		.y = 0,
-		.w = sprtiteSize,
-		.h = sprtiteSize
+		.w = spriteWidth,
+		.h = spritelength
 
 	};
 	SDL_FRect dest
@@ -208,11 +234,79 @@ void drawObject(const SDLstate &state, GameState& gs, GameObject& obj, float del
 		.y = obj.position.y ,
 		//.x = 100,
 		//.y = 200 - sprtiteSize,
-		.w = sprtiteSize,
-		.h = sprtiteSize
+		.w = spriteWidth,
+		.h = spritelength
 
 	};
 	SDL_FlipMode flipmode = (obj.direction == -1) ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
 
 	SDL_RenderTextureRotated(state.renderer,obj.texture, &src, &dest, 0, nullptr, flipmode);
+}
+
+void update(SDLstate &state, GameState& gs, GameObject &obj, resource &rs , float delta) {
+	if (obj.type == ObjectType::player)
+	{
+
+		float currentDirection = 0;
+		if (state.keys[SDL_SCANCODE_A]) {
+			currentDirection += -1;
+		}
+		if (state.keys[SDL_SCANCODE_D]) {
+			currentDirection += +1;
+		}
+		if (currentDirection) {
+			obj.direction = currentDirection;
+		}
+		switch (obj.data.player.state)
+		{
+			case PlayerState::idle:
+			{
+				if (currentDirection != 0)
+				{
+					obj.data.player.state = PlayerState::running;
+					obj.texture = rs.texRun;
+					obj.currentAnimations = rs.ANIM_PLAYER_RUN;
+					break;
+				}
+				else {
+					if (obj.velocity.x) {
+
+						const float factor = obj.velocity.x > 0 ? -1.5f : 1.5f;
+						float amount = factor * obj.acceleration.x * delta;
+						if (abs(obj.velocity.x) < abs(amount)) {
+							obj.velocity.x = 0;
+						}
+						else {
+							obj.velocity.x += amount;
+
+						}
+
+					}
+
+				}
+			}
+		
+			case PlayerState::running:
+			{
+				if (currentDirection == 0)
+				{
+					obj.data.player.state = PlayerState::idle;
+					
+					obj.texture = rs.texIdle;
+					obj.currentAnimations = rs.ANIM_PLAYER_IDLE;
+					break;
+				}
+			}
+
+	    }
+		obj.velocity += currentDirection * obj.acceleration * delta;
+		if (abs(obj.velocity.x) > obj.maxSpeed) {
+
+			obj.velocity.x = currentDirection * obj.maxSpeed ;
+
+		}
+		obj.position += obj.velocity * delta;
+	}
+
+
 }
--- gameObject.h
+++ gameObject.h
@@ -4,25 +4,55 @@
 #include"animation.h"
 #include<SDL3/SDL.h>
 
+
+enum class PlayerState {
+	idle , running , jumping
+};
+
 enum class ObjectType {
 	player , level , enemy
 };
+struct PlayerData
+{
 
+	PlayerState state;
+	PlayerData() {
+		state = PlayerState::idle;
+
+
+	}
+};
+struct LevelData {
+
+};
+struct EnemyData {
+
+};
+union ObjectData
+{
+	PlayerData player;
+	LevelData level;
+	EnemyData enemy;
+};
 
 struct GameObject 
 {
 
 	ObjectType type;
+	ObjectData data;
 	glm::vec2 position, velocity, acceleration;
 	float direction;
+	float maxSpeed;
 	std::vector<Animation> animations;
 	int currentAnimations;
 	SDL_Texture* texture;
 
 
-	GameObject() {
+	GameObject() :data{ .level = LevelData()}
+	{
 		type = ObjectType::level;
 		direction = 1;
+		maxSpeed=0;
 		position = velocity = acceleration = glm::vec2(0);
 		int currentAnimation = -1;
 		texture = nullptr;
